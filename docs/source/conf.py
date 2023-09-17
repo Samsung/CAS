f23ade313e0e690b508d9537cf7f112e021a5077
Adrian Niec <a.niec@samsung.com>
2023-09-17 09:15:45 +0200

    sphinx config cleanup

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -1,29 +1,15 @@
-# Configuration file for the Sphinx documentation builder.
-#
-# For the full list of built-in configuration values, see the documentation:
-# https://www.sphinx-doc.org/en/master/usage/configuration.html
-
-# -- Project information -----------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
-import os
 import sys
+import os
+
 project = 'CAS'
 copyright = '2022, a.niec@samsung.com'
 author = 'a.niec@samsung.com'
 release = '1.0'
 
-# -- General configuration ---------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
-
 extensions = ['sphinx.ext.autodoc']
 
 templates_path = ['_templates']
 exclude_patterns = ['test_main.py']
-
-
-
-# -- Options for HTML output -------------------------------------------------
-# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
 html_static_path = ['_static']
